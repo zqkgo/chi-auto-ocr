3cbda2d7545d1e86b4f0d0f6801aa86f1e31155b
zqk <zhangqiankun@qiniu.com>
2022-10-14 08:45:23 +0800

    remove \n and rewrite output file

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -36,7 +39,41 @@ func main() {
 				if err != nil {
 					log.Printf("failed to run tesseract, err: %+v\n", err)
 				}
+
 				handled[path] = true
+
+				// clear \n
+				outFile := fmt.Sprintf("%d.txt", cnt)
+				f, err := os.OpenFile(outFile, os.O_RDWR, 0)
+				if err != nil {
+					log.Printf("failed to open file %s, err: %v", outFile, err)
+					return nil
+				}
+				defer f.Close()
+				bs, err := ioutil.ReadAll(f)
+				if err != nil {
+					log.Printf("failed to read file %s, err: %v", outFile, err)
+					return nil
+				}
+				newContent := strings.Replace(string(bs), "\n", "", -1)
+				err = os.Truncate(outFile, 0)
+				if err != nil {
+					log.Printf("failed to truncate file %s, err: %v", outFile, err)
+					return nil
+				}
+				_, err = f.Seek(0, io.SeekStart)
+				if err != nil {
+					log.Printf("failed to seek file %s, err: %v", outFile, err)
+				}
+				n, err := f.WriteString(newContent)
+				if err != nil {
+					log.Printf("failed to write back to file %s, err: %v", outFile, err)
+					return nil
+				}
+				if n != len(newContent) {
+					log.Printf("not fully write, want: %d, write: %d", len(newContent), n)
+					return nil
+				}
 			}
 			return nil
 		})
